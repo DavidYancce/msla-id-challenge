26441d5ff1af097856a085f68b3a9dd50789cde7
David Yanccehuallpa <david.yanccehuallpa.q@uni.pe>
2025-06-30 21:04:49 -0500

    first commit

---
+++ src/main/java/msla/challenge/mslachallenge/MslaChallengeApplication.java
@@ -0,0 +1,13 @@
+package msla.challenge.mslachallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MslaChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MslaChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/msla/challenge/mslachallenge/MslaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package msla.challenge.mslachallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MslaChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
